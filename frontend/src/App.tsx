d1aec8dbccb38c217676b94e8340898591013182
leonj <jakleb20@htl-kaindorf.at>
2024-09-24 14:14:17 +0200

    init commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
           <Routes>
               <Route path={"/"} element={<Login></Login>}>
                   
+                  
               </Route>
           </Routes>
       </BrowserRouter>
