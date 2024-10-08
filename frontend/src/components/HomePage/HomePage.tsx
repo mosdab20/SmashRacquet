fa20efbac9566b1c3e9a5a16aea64176d6ab4f2a
leonj <jakleb20@htl-kaindorf.at>
2024-10-08 15:44:33 +0200

    final commit

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -1,28 +1,11 @@
-// import React from 'react';
 import './HomePage.css';
-import {useEffect, useState} from "react";
-//import {User} from "../../interface/User.tsx";
-
-import {Tournament} from "../../interface/Tournament.tsx";
-import {TournamentService} from "../../Service/TournamentService.ts";
 
 const HomePage = () => {
-    //const [users, setUsers] = useState<User[]>([]);
-    //const [matches , setMatches] = useState<Match[]>([]);
-    const [tournaments , setTournaments] = useState<Tournament[]>([]);
 
-    useEffect(() => {
-        TournamentService.getTournaments().then(setTournaments)
-    },[])
-    // const navigate = useNavigate();
-    
-    
     return (
 
         <div>
-            {tournaments.map((tou: Tournament) => (
-                <div>{tou.description}</div>
-            ))}
+            
         </div>
     );
 };
