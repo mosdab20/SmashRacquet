8030b882b19dd05a09e0ce2c2ca17d35a9032e26
bogbeb19 <94896905+bogbeb19@users.noreply.github.com>
2024-10-08 16:06:09 +0200

    Turniere anzeigen

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -1,14 +1,38 @@
-// import React from 'react';
 import './HomePage.css';
 
 const HomePage = () => {
-    
-    // const navigate = useNavigate();
-    
-    
     return (
-        <div>
-            <div>hallo </div>
+        <div className="homepage">
+            <main className="content">
+                <section className="live-score">
+                    <div className="text-container">
+                        <h2>Live</h2>
+                        <h3>Klasse 1 vs. Klasse 2</h3>
+                        <div className="score">3:1</div>
+                    </div>
+                </section>
+
+                <section className="next-matches">
+                    <h2>Nächste Spiele</h2>
+                    <div className="matches-grid">
+                        <div className="match-card">
+                            <time>10:30</time>
+                            <p>Klasse 3 vs. Klasse 4</p>
+                            <span>Gruppe A</span>
+                        </div>
+                        <div className="match-card">
+                            <time>10:45</time>
+                            <p>Klasse 1 vs. Klasse 4</p>
+                            <span>Gruppe B</span>
+                        </div>
+                        <div className="match-card">
+                            <time>12:00</time>
+                            <p>Klasse 5 vs. offen</p>
+                            <span>Achtelfinale</span>
+                        </div>
+                    </div>
+                </section>
+            </main>
         </div>
     );
 };
