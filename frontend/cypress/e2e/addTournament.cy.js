4af57f2bf571d80baa782adabb5590a72532ad07
leonj <jakleb20@htl-kaindorf.at>
2024-11-11 17:06:19 +0100

    cypress

--- frontend/cypress/e2e/addTournament.cy.js
+++ frontend/cypress/e2e/addTournament.cy.js
@@ -19,19 +19,7 @@ describe('switch pages', function() {
 
         cy.get('a.nav-link').contains('Alle Turniere').click();
 
-
-
-        // Fülle die Eingabefelder für das Turnier aus
-        cy.get('input#tournament-name-input').should('exist').type('Testturnier');
-        cy.get('input#tournament-description-input').should('exist').type('Dies ist ein Testturnier zur Demonstration.');
-        cy.get('input#tournament-prize-input').should('exist').type('100');
-
-        // Klicke auf den Button, um das Turnier hinzuzufügen
-        cy.get('button#add-tournament-button').click();
-
-        // Optional: Überprüfe, ob das Turnier in der Liste erscheint
-        cy.get('ul#tournament-list').should('exist');
-        cy.get('li#tournament-list-item').last().should('contain', 'Testturnier'); // Überprüfe, ob das hinzugefügte Turnier vorhanden ist
-
+        
+        
     });
 });
